585f16a46dda2524eef3bee48cbd2226f9a9511e
jhoon87 <38357002+jhoon87@users.noreply.github.com>
2024-12-28 11:23:02 +0900

    market_news.py

--- market_news.py
+++ market_news.py
@@ -25,6 +25,8 @@ def setup_driver():
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.binary_location = '/usr/bin/google-chrome'
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
